d8ea857ae0122398bc1365d0067259d4641f8068
Shinigami92 <chrissi92@hotmail.de>
2022-07-09 00:54:25 +0200

    feat: add uuid generator

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,12 @@
 use std::fs::File;
 use std::io::prelude::*;
+use uuid::Uuid;
 
 fn main() -> std::io::Result<()> {
+    let id = Uuid::new_v4();
+
     let mut file = File::create("foo.txt")?;
-    file.write_all(b"Hello, world!")?;
+    let content = "Hello, world!".to_owned() + "\n" + &id.to_string();
+    file.write_all(content.as_bytes())?;
     Ok(())
 }

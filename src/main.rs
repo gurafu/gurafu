f4764bf6303e709b23ec26e189980d4a5bf3c8e0
Shinigami92 <chrissi92@hotmail.de>
2022-07-09 01:17:26 +0200

    feat: write file into a collections folder

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use std::fs;
 use std::fs::File;
 use std::io::prelude::*;
 use uuid::Uuid;
@@ -5,7 +6,14 @@ use uuid::Uuid;
 fn main() -> std::io::Result<()> {
     let id = Uuid::new_v4();
 
-    let mut file = File::create("foo.txt")?;
+    let first_two = &id.to_string()[..2];
+    let path = format!("{}{}{}{}", "collections/", first_two, "/", id);
+
+    print!("{}", path);
+
+    fs::create_dir_all("collections/".to_owned() + first_two)?;
+
+    let mut file = File::create(path)?;
     let content = "Hello, world!".to_owned() + "\n" + &id.to_string();
     file.write_all(content.as_bytes())?;
     Ok(())

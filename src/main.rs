2a9b04149448d7be32385d7d4e8cec719e260a4d
Shinigami92 <chrissi92@hotmail.de>
2022-07-08 22:27:23 +0200

    feat: write into a file

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,8 @@
-fn main() {
-    println!("Hello, world!");
+use std::fs::File;
+use std::io::prelude::*;
+
+fn main() -> std::io::Result<()> {
+    let mut file = File::create("foo.txt")?;
+    file.write_all(b"Hello, world!")?;
+    Ok(())
 }

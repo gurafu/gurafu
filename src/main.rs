ce977bdd54b41d5cf1776e791a53baafa120098c
Shinigami92 <chrissi92@hotmail.de>
2022-07-09 02:16:17 +0200

    feat: define a schema builder

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,37 @@ use std::fs::File;
 use std::io::prelude::*;
 use uuid::Uuid;
 
+pub mod gurafu {
+    pub mod types {
+        pub const TEXT: &'static str = "Text";
+        pub const TIMESTAMP: &'static str = "Timestamp";
+    }
+
+    pub struct SchemaBuilder {}
+
+    impl SchemaBuilder {
+        pub fn new() -> SchemaBuilder {
+            println!("{}", "new");
+            SchemaBuilder {}
+        }
+
+        pub fn create_vertex(&mut self, name: &str) -> &mut SchemaBuilder {
+            println!("{}", name);
+            self
+        }
+
+        pub fn property(&mut self, name: &str, typ: &'static str) -> &mut SchemaBuilder {
+            println!("{} {}", name, typ);
+            self
+        }
+
+        pub fn create(&mut self) -> () {
+            println!("{}!", "create");
+            ()
+        }
+    }
+}
+
 fn main() -> std::io::Result<()> {
     let id = Uuid::new_v4();
 
@@ -16,5 +47,14 @@ fn main() -> std::io::Result<()> {
     let mut file = File::create(path)?;
     let content = "Hello, world!".to_owned() + "\n" + &id.to_string();
     file.write_all(content.as_bytes())?;
+
+    gurafu::SchemaBuilder::new()
+        .create_vertex("user")
+        .property("username", gurafu::types::TEXT)
+        .property("password", gurafu::types::TEXT)
+        .property("lastLoggedIn", gurafu::types::TIMESTAMP)
+        .property("createdAt", gurafu::types::TIMESTAMP)
+        .create();
+
     Ok(())
 }

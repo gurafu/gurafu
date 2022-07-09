8a897b716a024e1d0388fd04db77250e0e570c47
Shinigami92 <chrissi92@hotmail.de>
2022-07-09 11:15:35 +0200

    feat: write example vertices file structure

--- src/main.rs
+++ src/main.rs
@@ -8,19 +8,36 @@ use std::io::prelude::*;
 use uuid::Uuid;
 
 fn main() -> std::io::Result<()> {
-    let id = Uuid::new_v4();
+    let database_name = "my_test_db";
 
-    let first_two = &id.to_string()[..2];
-    let path = format!("{}{}{}{}", "collections/", first_two, "/", id);
+    let path_to_db = format!("{}/{}", "gurafu", database_name);
 
-    print!("{}", path);
+    // Create database
+    fs::create_dir_all(&path_to_db)?;
 
-    fs::create_dir_all("collections/".to_owned() + first_two)?;
+    // Create "user" vertex definition
+    let path_to_user_vertex = format!("{}/{}/{}", path_to_db, "vertices", "user");
+    fs::create_dir_all(&path_to_user_vertex)?;
 
-    let mut file = File::create(path)?;
-    let content = "Hello, world!".to_owned() + "\n" + &id.to_string();
-    file.write_all(content.as_bytes())?;
+    let mut user_definition_file =
+        File::create(format!("{}/{}", path_to_user_vertex, "definition"))?;
+    user_definition_file
+        .write_all(b"username,Text\npassword,Text\nlastLoggedIn,Timestamp\ncreatedAt,Timestamp")?;
 
+    // Insert a user
+    let id = Uuid::new_v4().simple().to_string();
+
+    let first_two_chars = &id[..2];
+    let path_to_user = format!("{}/{}", path_to_user_vertex, first_two_chars);
+
+    fs::create_dir_all(&path_to_user)?;
+
+    let rest_of_id = &id[2..];
+
+    let mut user_file = File::create(format!("{}/{}", path_to_user, rest_of_id))?;
+    user_file.write_all(b"Shinigami\n$2a$13$mhQRlZqBqPUbkThjgBp1r.ftgpzG54ra4mTCS0acigwwk1xwUMH1q\n\n2022-07-09T08:47:45.409Z")?;
+
+    // For later
     SchemaBuilder::new()
         .create_vertex("user")
         .property("username", types::TEXT)

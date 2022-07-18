542e364abecd1e8868d2ac3d3720fbcda1010ce1
Shinigami92 <chrissi92@hotmail.de>
2022-07-18 17:20:50 +0800

    docs: add doc

--- src/gurafu/query/step.rs
+++ src/gurafu/query/step.rs
@@ -2,6 +2,8 @@ use uuid::Uuid;
 
 #[derive(Clone, PartialEq)]
 pub enum QueryStep {
+    /// * `name` - The name of the vertex to find.
     FindVertex(String),
+    /// * `id` - The id of the vertex to find.
     WithId(Uuid),
 }
